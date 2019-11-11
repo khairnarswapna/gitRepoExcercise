431c9361ffa32980406cbc79d0ef0521af194824
mahendra kundare <mr.kundare@gmail.com>
2019-11-11 17:40:49 +0530

    coding changes made in usecase3

--- usecase.sh
+++ usecase.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 read -p "Enter value of a : " a 
 read -p "Enter value of b : " b
 read -p "Enter value of c : " c
@@ -6,6 +6,36 @@ read -p "Enter value of c : " c
 ans=$((a+b*c))
 echo "$a + $b * $c = $ans"
 
-ans=$((c+a/b))
-echo "$c + $a / $b =$ans "
+ans1=$((c+a/b))
+echo "$c + $a / $b =$ans1"
+
+ans2=$((a%b+c))
+echo="$a % $b + $c=$ans2"
+
+function operation()
+{
+    result=$(($1+$2*$3))
+    echo $result
+}
+
+function operation2()
+{
+    result=$(($1+$2/$3))
+    echo $result
+}
+
+function operation3()
+{
+     result=$(($1%$2+$3))
+     echo $result
+}
+
+result1="$( operation $(($a)) $(($b)) $(($c)) )"
+result2="$( operation2 $(($a)) $(($b)) $(($c)) )"
+result3="$( operation3 $(($a)) $(($b)) $(($c)) )"
+
+echo $result1
+echo $result2
+echo $result3
+
 
